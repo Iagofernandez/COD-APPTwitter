4d95f6f87bf1748ddf5b76ae994bcdd04d0af9a8
ifernandezblanco <ifernandezblanco@nautilus29.danielcastelao.org>
2019-04-09 10:03:06 +0200

    Twitter escribir, linea de tiempo y busqueda

--- src/twitterjavagt/TwitterAP.java
+++ src/twitterjavagt/TwitterAP.java
@@ -5,7 +5,6 @@
  */
 package twitterjavagt;
 
-import java.io.IOException;
 import java.util.List;
 import twitter4j.Query;
 import twitter4j.QueryResult;
@@ -13,8 +12,6 @@ import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
-import twitter4j.auth.AccessToken;
-import twitter4j.auth.RequestToken;
 import twitter4j.conf.ConfigurationBuilder;
 
 /**
@@ -23,37 +20,48 @@ import twitter4j.conf.ConfigurationBuilder;
  */
 public class TwitterAP {
         public static void main(String[] args) throws TwitterException {
-
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true);
+        TwitterFactory tf = new TwitterFactory(cb.build());
+        Twitter twitter = tf.getInstance();
     
-         // importacion 
-        //objeto clase twitter, para postear
-        // el sigleton nos permite instanciar un solo twitter
-        Twitter twitter = TwitterFactory.getSingleton();
-        //creacion de la variable latestStatus
-        //se inicia en null
-            String latestStatus = "hola";
+        /**
+         * @author ifernandezblanco
+        * importacion 
+        *objeto clase twitter, para postear
+        * el sigleton nos permite instanciar un solo twitter
+        */
+        Twitter twitter1 = TwitterFactory.getSingleton();
+        /**creacion de la variable latestStatus
+        *se inicia dandole un valor que escribira en Twitter
+         */
+            String latestStatus = "hola chicos";
   Status status = twitter.updateStatus(latestStatus);
   System.out.println("Se actualizo el estado [" + status.getText() + "].");
   
-        
- Twitter twitter1 = TwitterFactory.getSingleton();
-    List<Status> statuses = twitter.getHomeTimeline();
-    System.out.println("Linea de tiempo");
-    for (Status statu : statuses) {
-        System.out.println(statu.getUser().getName() + ":" +
-                           statu.getText());
+          /**Esto nos permite ver la linea de tiempo de todo tu Twitter
+           * List Status que nos da linea de tiempo
+           * 
+           */
+// Twitter twitter1 = TwitterFactory.getSingleton();
+//    List<Status> statuses = twitter.getHomeTimeline();
+//    System.out.println("Linea de tiempo");
+//    for (Status statu : statuses) {
+//        System.out.println(statu.getUser().getName() + ":" +
+//                           statu.getText());
+//    }
+//    
+//    Twitter twitter2 = TwitterFactory.getSingleton();
+//    Query query = new Query("source:twitter4j yusukey");
+//    QueryResult result = twitter.search(query);
+//    for (Status statu : result.getTweets()) {
+//        System.out.println("@" + statu.getUser().getScreenName() + ":" + statu.getText());
+//    }
+//    
+//     
+//        }
     }
-    
-    Twitter twitter2 = TwitterFactory.getSingleton();
-    Query query = new Query("source:twitter4j yusukey");
-    QueryResult result = twitter.search(query);
-    for (Status statu : result.getTweets()) {
-        System.out.println("@" + statu.getUser().getScreenName() + ":" + statu.getText());
-    }
-    
-     
-        }
-    }
+}
 
         
     
